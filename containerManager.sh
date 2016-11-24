9d8cd5878e42f671910ddcab0f1aa7d7d81f3b0f
Bart Willems <ps3bartwillems@gmail.com>
2016-11-24 17:22:48 +0100

    Build & start containers

--- containerManager.sh
+++ containerManager.sh
@@ -1,9 +1,11 @@
-#!/bin/bash 
+#!/bin/bash
 
 if [ "$EUID" -ne 0 ]
     then echo "You should run this script as root"
     exit 1
 fi
+
+LOCATION='/opt/docker-ubuntu-vnc-desktop'
 
 while [[ $# > 1 ]]
 do
@@ -27,3 +29,25 @@ do
     esac
     shift
 done
+
+# Exit when these vars are empty
+[ ! -z "$NAME" ] || exit 1
+[ ! -z "$PORT" ] || exit 1
+
+if [ "$ACTION" == 'add' ];then
+  [ ! -z "$PASSWORD" ] || exit 1
+  sed -i 's,^\(PASS=\).*,\1'${PASSWORD}',' $LOCATION/startup.sh
+  docker build --rm -t dorowu/ubuntu-desktop-lxde-vnc-$NAME-$PORT docker-ubuntu-vnc-desktop
+  if [ ! "$?" -eq 0 ]; then
+    exit $?
+  fi
+
+  docker run -i -t -p $PORT:6080 dorowu/ubuntu-desktop-lxde-vnc-$NAME-$PORT
+  if [ ! "$?" -eq 0 ]; then
+    exit $?
+  fi
+  sed -i 's,^\(PASS=\).*,\1'_EMPTY_',' $LOCATION/startup.sh
+  exit 0
+elif [ "$ACTION" == 'remove' ];then
+  # To be implemented
+fi
